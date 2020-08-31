a3ca8874d0b3e065c3b450bb49074dbb5262d342
Anna Vlasova <annasv209@gmail.com>
2020-08-31 11:27:04 +0300

    Fix python scripts

---
+++ scripts/data_loader.py
@@ -0,0 +1,44 @@
+import os
+import json
+import pandas as pd
+
+
+def load():
+    dirname = os.path.dirname(__file__)
+    data_dirname = os.path.join(dirname, 'build_info')
+
+    df_result = pd.DataFrame()
+    for filename in os.listdir(data_dirname):
+        builds_info_file = open(os.path.join(data_dirname, filename), 'r')
+        builds_info = json.loads(builds_info_file.read())
+        builds_info_file.close()
+
+        df = convert_to_data_frame(builds_info)
+        df_result = pd.concat([df_result, df], axis=0)
+
+    df_result.to_csv('investigations.csv', index=False)
+
+def convert_to_data_frame(builds_info):
+    result = {}
+    for key in all_keys:
+        result[key] = []
+    for build in builds_info:
+        correct_tests = list(filter(lambda test: "previousResponsible" in test, build['tests']))
+        for test in correct_tests:
+            row = {**build, **test}
+            for key in all_keys:
+                if key not in row:
+                    result[key].append(None)
+                else:
+                    result[key].append(row[key])
+
+    return pd.DataFrame(result)
+
+
+all_keys = ['buildId', 'clientDate', 'committers', 'changeCount', 'comment', 'triggeredBy', 'description',
+            'isDefaultBranch', 'branchName', 'committersUsers', 'reasons', 'testCount', 'testRunId',
+            'testNameId', 'stacktrace', 'className', 'testMethod', 'testClass', 'testSuite',
+            'duration', 'orderId', 'isFixed', 'previousResponsible']
+
+if __name__ == '__main__':
+    load()
--- scripts/requests_manager.py
+++ scripts/requests_manager.py
@@ -7,38 +7,85 @@ import time
 
 
 def load_build_ids(host, project_id, token, timeout=10800):
-    response = requests.get(f'{host}/app/rest/builds/?locator=affectedProject:{project_id}',
+    response = requests.get(f'{host}/app/rest/builds/?locator=affectedProject:{project_id},start:11300,count:100',
                             headers={'Authorization': token, 'Accept': 'application/json'}, timeout=timeout)
 
     response_json = json.loads(response.text)
-    build_ids = str(list(map(lambda t: t['id'], response_json['build'])))
+    build_ids = list(map(lambda t: t['id'], response_json['build']))
+    i = 0
+    while "nextHref" in response_json and i < 10:
+        i += 1
+        print(i)
+        response = requests.get(f'{host}{response_json["nextHref"]}',
+                                headers={'Authorization': token, 'Accept': 'application/json'}, timeout=timeout)
+        response_json = json.loads(response.text)
+        build_ids += list(map(lambda t: t['id'], response_json['build']))
+
     dirname = os.path.dirname(__file__)
     file_ids = open(os.path.join(dirname, 'build_ids.json'), 'w')
-    file_ids.write(build_ids)
+    file_ids.write(str(build_ids))
     file_ids.close()
     logging.info(f'Build ids received')
 
 
+def load_build_test_ids(host, project_id, token, timeout=10800):
+    response = requests.get(f'{host}/buildTestIdsCollector.html',
+                            params={'projectExternalId': project_id},
+                            headers={'Authorization': token}, timeout=timeout)
+
+    response_json = json.loads(response.text)
+    dirname = os.path.dirname(__file__)
+    file_ids = open(os.path.join(dirname, 'build_test_ids.json'), 'w')
+    file_ids.write(str(response_json))
+    file_ids.close()
+    logging.info(f'Build test ids received')
+
+
 def load_builds_info(host, project_id, token, start_from=0, batch_size=5, timeout=10800):
     dirname = os.path.dirname(__file__)
-    file_ids = open(os.path.join(dirname, 'build_ids.json'), 'r')
-    build_ids = json.loads(file_ids.read())
+    file_ids = open(os.path.join(dirname, 'build_test_ids.json'), 'r')
+    build_test_ids = json.loads(file_ids.read())
     file_ids.close()
 
-    for i in range(start_from, len(build_ids), batch_size):
-        batch = build_ids[i:i + batch_size]
+    for i in range(start_from, len(build_test_ids), batch_size):
+        batch = build_test_ids[i:i + batch_size]
 
         time_before = time.time()
         response = requests.get(f'{host}/assignInfoCollector.html',
-                                params={'projectExternalId': project_id, 'ids': ','.join(map(str, batch))},
+                                params={'projectExternalId': project_id,
+                                        'ids': ','.join(map(lambda pair: f'{pair["buildId"]}_{pair["testId"]}', batch))},
                                 headers={'Authorization': token}, timeout=timeout)
+
         if not response.ok:
             logging.info(f'Error: status code {response.status_code}')
         time_after = time.time()
-        file_info = open(os.path.join(dirname, f'builds_info_{i}_{i + batch_size - 1}.json'), 'w')
+        file_info = open(os.path.join(dirname, 'build_info', f'builds_info_{i}_{i + batch_size - 1}.json'), 'w')
         file_info.write(response.text)
         file_info.close()
         logging.info(f'Finished batch from {i} to {i + batch_size - 1} in {time_after - time_before} seconds')
+
+
+# def load_builds_info(host, project_id, token, start_from=0, batch_size=5, timeout=10800):
+#     dirname = os.path.dirname(__file__)
+#     file_ids = open(os.path.join(dirname, 'build_ids.json'), 'r')
+#     build_ids = json.loads(file_ids.read())
+#     file_ids.close()
+#
+#     for i in range(start_from, len(build_ids), batch_size):
+#         batch = build_ids[i:i + batch_size]
+#
+#         time_before = time.time()
+#         response = requests.get(f'{host}/assignInfoCollector.html',
+#                                 params={'projectExternalId': project_id, 'ids': ','.join(map(str, batch))},
+#                                 headers={'Authorization': token}, timeout=timeout)
+#
+#         if not response.ok:
+#             logging.info(f'Error: status code {response.status_code}')
+#         time_after = time.time()
+#         file_info = open(os.path.join(dirname, 'build_info', f'builds_info_{i}_{i + batch_size - 1}.json'), 'w')
+#         file_info.write(response.text)
+#         file_info.close()
+#         logging.info(f'Finished batch from {i} to {i + batch_size - 1} in {time_after - time_before} seconds')
 
 
 def parse_args():
@@ -65,7 +112,7 @@ def main(args):
     logging.basicConfig(filename='data_loader.log', level=logging.DEBUG)
 
     if args.request_type == 'build_ids':
-        load_build_ids(args.host, args.project_id, args.auth_token)
+        load_build_test_ids(args.host, args.project_id, args.auth_token)
     elif args.request_type == 'builds_info':
         load_builds_info(args.host, args.project_id, args.auth_token,
                          start_from=int(args.start_from),
